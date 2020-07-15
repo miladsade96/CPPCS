fa05d8fa1d7f66b39aefa4d76a5190b941927ce8
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-15 23:38:40 +0430

    getting user input

--- src/numerical_methods/Exercise_01.cpp
+++ src/numerical_methods/Exercise_01.cpp
@@ -10,5 +10,8 @@ using namespace std;
 int main(){
     // declaring variables
     double I, E, R, L, C, f;
+    cout << "Enter values for E, R, L, C, and f respectively: ";
+    // getting user input
+    cin >> E >> R >> L >> C >> f;
 
 }
