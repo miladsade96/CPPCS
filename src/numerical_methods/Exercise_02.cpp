3c22303f2be7bf632514b8411ce0c4b9f7fc5392
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-18 14:43:18 +0430

    converting degree to radian

--- src/numerical_methods/Exercise_02.cpp
+++ src/numerical_methods/Exercise_02.cpp
@@ -10,4 +10,6 @@ using namespace std;
 int main(){
     // declaring and initializing variables
     double w = 513.0, h = 3.0, phi = 30, radian_phi, P;
+    // converting degree to radian
+    radian_phi = phi * 3.1415 / 180.0;
 }
