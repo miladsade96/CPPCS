2dbd1ee7789f2f0773adee267467c923824e74c3
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-18 14:44:15 +0430

    calculating P and printing result

--- src/numerical_methods/Exercise_02.cpp
+++ src/numerical_methods/Exercise_02.cpp
@@ -12,4 +12,7 @@ int main(){
     double w = 513.0, h = 3.0, phi = 30, radian_phi, P;
     // converting degree to radian
     radian_phi = phi * 3.1415 / 180.0;
+    //calculating P and printing result
+    P = (w * pow(h, 2.0) / 2.0) * ((1.0 - sin(radian_phi)) / (1.0 + sin(radian_phi)));
+    cout << "P: " << P;
 }
