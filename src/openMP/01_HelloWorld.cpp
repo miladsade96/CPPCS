3ea10bf3ba7e41a177f24f755dc3a839b7757b7f
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-20 08:53:04 +0430

    updating 01_HelloWorld.cpp

--- src/openMP/01_HelloWorld.cpp
+++ src/openMP/01_HelloWorld.cpp
@@ -9,8 +9,12 @@
 using namespace std;
 
 int main(){
+    // setting the number of threads in parallel region
+    omp_set_num_threads(4);
 #pragma omp parallel
     {
-        cout << "HelloWorld!";
+        // getting current thread number
+        int ID = omp_get_thread_num();
+        cout << "HelloWorld! , ID: " << ID << endl;
 };
 }
